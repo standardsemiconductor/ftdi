2b144702b8a223c4864ad6a7ee82b2c41ad80e8b
dopamane <dwc1295@gmail.com>
2020-06-17 21:21:55 +0000

    no testing for now, needs rework

--- default.nix
+++ default.nix
@@ -1,2 +1,13 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc883" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./ftdi.nix { }
+{ mkDerivation, async, base, base-unicode-symbols, bytestring
+, stdenv, transformers, usb, vector
+}:
+mkDerivation {
+  pname = "ftdi";
+  version = "1.0.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    async base base-unicode-symbols bytestring transformers usb vector
+  ];
+  description = "A thin layer over USB to communicate with FTDI chips";
+  license = stdenv.lib.licenses.bsd3;
+}
