1ea950c1248bcf3fb5365d3696ad0366421d64c4
dopamane <dwc1295@gmail.com>
2025-11-15 22:57:05 -0800

    Make sub-command

--- exe/Main.hs
+++ exe/Main.hs
@@ -4,8 +4,15 @@ import Options.Applicative
 import System.FTDI
 import qualified System.USB as USB
 
-data Cli
-  = SetBaudRate USB.VendorId USB.ProductId ChipType Interface Integer
+newtype Cli = SetBaudCmd SetBaud
+
+data SetBaud
+  = SetBaud
+      USB.VendorId
+      USB.ProductId
+      ChipType
+      Interface
+      Integer -- ^ baud rate
 
 main :: IO ()
 main = ftdi =<< runCli
@@ -16,22 +23,27 @@ runCli = customExecParser prefs' $ info (cliParser <**> helper) mempty
     prefs' = prefs $ showHelpOnError <> showHelpOnEmpty
 
 cliParser :: Parser Cli
-cliParser =
-  SetBaudRate
-    <$> vendorIdOptParser
-    <*> productIdOptParser
-    <*> chipTypeOptParser
-    <*> interfaceOptParser
-    <*> baudOptParser
+cliParser = hsubparser $ mconcat
+  [ command "set-baud" $ info (SetBaudCmd <$> setBaudParser) mempty
+  ]
 
-vendorIdOptParser :: Parser USB.VendorId
-vendorIdOptParser = option auto $ long "vid" <> help "Vendor ID"
+setBaudParser :: Parser SetBaud
+setBaudParser =
+  SetBaud
+    <$> vendorIdParser
+    <*> productIdParser
+    <*> chipTypeParser
+    <*> interfaceParser
+    <*> baudParser
 
-productIdOptParser :: Parser USB.ProductId
-productIdOptParser = option auto $ long "pid" <> help "Product ID"
+vendorIdParser :: Parser USB.VendorId
+vendorIdParser = option auto $ long "vid" <> help "Vendor ID"
 
-chipTypeOptParser :: Parser ChipType
-chipTypeOptParser = asum
+productIdParser :: Parser USB.ProductId
+productIdParser = option auto $ long "pid" <> help "Product ID"
+
+chipTypeParser :: Parser ChipType
+chipTypeParser = asum
   [ flag' ChipType_AM    $ long "AM"
   , flag' ChipType_BM    $ long "BM"
   , flag' ChipType_2232C $ long "2232C"
@@ -40,19 +52,19 @@ chipTypeOptParser = asum
   , flag' ChipType_4232H $ long "4232H"
   ]
 
-interfaceOptParser :: Parser Interface
-interfaceOptParser = asum
+interfaceParser :: Parser Interface
+interfaceParser = asum
   [ flag' Interface_A $ short 'A'
   , flag' Interface_B $ short 'B'
   , flag' Interface_C $ short 'C'
   , flag' Interface_D $ short 'D'
   ]
 
-baudOptParser :: Parser Integer
-baudOptParser = option auto $ long "baud" <> help "Baud rate"
+baudParser :: Parser Integer
+baudParser = option auto $ long "baud" <> help "Baud rate"
 
 ftdi :: Cli -> IO ()
-ftdi (SetBaudRate vid pid chipType iface baud) = do
+ftdi (SetBaudCmd (SetBaud vid pid chipType iface baud)) = do
   devices <- USB.getDevices =<< USB.newCtx
   selects <- filterM (selectDevice vid pid) $ toList devices
   case selects of
